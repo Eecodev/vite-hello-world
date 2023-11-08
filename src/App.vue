a79a44aa8f12b10db74451b9598c1ede1b8c8c09
Eecodev <mnasser.bbk@gmail.com>
2023-11-08 16:17:26 +0100

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import AppTitle from './components/AppTitle.vue'
+
+
+</script>
+
+<template>
+  <HelloWorld />
+  <AppTitle />
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+
+
+<template>
+  <h1>
+    ciao!
+  </h1>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
