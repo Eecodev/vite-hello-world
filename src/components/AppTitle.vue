3d7dda02cb309afd180f56a7496a45d1cbb3c9af
Eecodev <mnasser.bbk@gmail.com>
2023-11-08 19:15:26 +0100

    text animation added

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,6 +1,7 @@
 <template>
-    <h1>
-       Mirror Image
+    <h1 class="mr">
+       <span class="word">Mirror</span>
+       <span class="word">Image</span>
     </h1>
     <div>
         <img src="/public/marc-olivier-jodoin--TQUERQGUZ8-unsplash.jpeg" alt="">
@@ -8,6 +9,7 @@
 </template>
 
 <script>
+import anime from 'animejs';
     export default {
         data(){
             return{
@@ -16,17 +18,46 @@
             
         },
         methods:{
+        animateTitle(){
+                const timeline = anime.timeline({loop: true})
+                .add({
+                    targets: '.mr .word',
+                    scale: [14,1],
+                    opacity: [0,1],
+                    easing: "easeOutCirc",
+                    duration: 800,
+                    delay: (el, i) => 800 * i
+                })
+                .add({
+                    targets: '.mr',
+                    opacity: 0,
+                    duration: 1000,
+                    easing: "easeOutExpo",
+                    delay: 1000
+                });
 
-        }
-        
+                timeline.play()
+                
+            }
+        },
+        mounted() {
+            this.animateTitle();
+        },    
     };
 </script>
 
 <style lang="scss" scoped>
-    h1{
+    .mr {
+        font-weight: 800;
+        font-size: 3.8em;
+        text-transform: uppercase;
+        letter-spacing: 0.5em;
         text-align: center;
-        margin-top: 20px;
-        font-size: 100px;
+        margin-top: 100px;
+    }
+    .mr .word {
+        display: inline-block;
+        line-height: 1em;
     }
     img{
         width: 500px;
