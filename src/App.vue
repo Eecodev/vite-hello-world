c4626e9f65490170b007c73ab1ba7766707705d9
Eecodev <mnasser.bbk@gmail.com>
2023-11-08 17:07:35 +0100

    AppTitle component created

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import AppTitle from './components/AppTitle.vue'
-
-
 </script>
 
 <template>
-  <HelloWorld />
   <AppTitle />
 </template>
 
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,13 +1,38 @@
 <template>
-    
+    <h1>
+       Mirror Image
+    </h1>
+    <div>
+        <img src="/public/marc-olivier-jodoin--TQUERQGUZ8-unsplash.jpeg" alt="">
+    </div>
 </template>
 
 <script>
     export default {
+        data(){
+            return{
+               name: 'AppTitle',
+            }
+            
+        },
+        methods:{
+
+        }
         
-    }
+    };
 </script>
 
 <style lang="scss" scoped>
-
+    h1{
+        text-align: center;
+        margin-top: 20px;
+        font-size: 100px;
+    }
+    img{
+        width: 500px;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 20px;
+    }
 </style>
